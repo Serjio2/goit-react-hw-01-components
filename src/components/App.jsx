e339ff1d4b178504a3730ef2aaaa76aea13622a3
Serhii Kondratenko <kondratenko.serhii3@gmail.com>
2023-08-05 01:52:24 +0300

    begin task 4

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,12 +1,14 @@
 import user from "../user.json";
 import data from "../data.json";
 import friends from "../friends.json";
+import transactions from "../transactions.json";
 
 import { GlobalStyle } from "./GlobalStyle";
 import { Profile } from "./Profile/Profile";
 import { Statistics } from "./Statistics/Statistics";
 import { FriendList } from "./FriendList/FriendList";
 import { Container } from "./Container.styled";
+import { TransactionHistory } from "./Transactions/Transactions";
 
 export const App = () => {
   return (
@@ -15,6 +17,7 @@ export const App = () => {
       <Profile user={user}/>
       <Statistics stats={data} title="Upload stats"/>
       <FriendList friends={friends}/>
+      <TransactionHistory items={transactions}/>
     </Container>
   );
 };
